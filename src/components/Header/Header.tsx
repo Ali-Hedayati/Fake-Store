603e4949bc2c8ac099b74f72ed95c368bc4011bd
Ali-Hedayati <a.hedayati.developer@gmail.com>
2024-06-27 12:34:51 +0330

    Searchbar is removed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,14 @@ import { FcShop } from "react-icons/fc";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 import { HStack } from "@chakra-ui/react";
-import SearchInput from "./SearchInput";
+
+
 
 const Header = () => {
   return (
     <HStack justify="space-between">
       <FcShop size={70} />
-      <SearchInput />
+     
       <ColorModeSwitch />
     </HStack>
   );
